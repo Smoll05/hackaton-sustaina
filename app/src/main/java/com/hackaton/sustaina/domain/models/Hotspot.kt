9e16a0dc78d1d2b91e966bb0cf9d043426d27a2a
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-03-01 22:20:08 +0800

    Modified the Hotspot class by adding new fields, including description, timestamp, statusLevel, densityLevel, imageUrl, and reportedByUserId, and setting a default radius.

--- app/src/main/java/com/hackaton/sustaina/domain/models/Hotspot.kt
+++ app/src/main/java/com/hackaton/sustaina/domain/models/Hotspot.kt
@@ -4,5 +4,11 @@ data class Hotspot(
     val hotspotId: String = "",
     val latitude: Double = 0.0,
     val longitude: Double = 0.0,
-    val radius: Int = 0
+    val description: String = "",
+    val timestamp: Long = 0,
+    val radius: Double = 20.0, // default to 20
+    val statusLevel: Int = 0,
+    val densityLevel: Int = 0,
+    val imageUrl: String = "",
+    val reportedByUserId: String = ""
 )
