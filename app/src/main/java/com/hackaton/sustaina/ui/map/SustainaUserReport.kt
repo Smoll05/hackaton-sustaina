15601940c399a341f4d63ee183abcf0cc03850e3
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-03-01 10:31:43 +0800

    Added user reports with coordinates and updated existing data.

--- app/src/main/java/com/hackaton/sustaina/ui/map/SustainaUserReport.kt
+++ app/src/main/java/com/hackaton/sustaina/ui/map/SustainaUserReport.kt
@@ -20,6 +20,8 @@ class SustainaUserReports {
         UserReport("report_125", 10.297658, 123.882134, "https://example.com/garbage3.jpg", "TRES DE ABRIL Scattered debris after market day", "In Progress", 1709057200, "user_321",3),
         UserReport("report_126", 10.297407, 123.896274, "https://example.com/garbage4.jpg", "UC MAIN Abandoned garbage bags on sidewalk", "Cleaned", 1709057400, "user_654",3),
         UserReport("report_127", 10.258640,  123.823828, "https://example.com/garbage5.jpg", "STAR MALL Improperly disposed food waste", "Uncleaned", 1709057600, "user_987",2),
+        UserReport("report_128", 10.294067, 123.882511, "https://example.com/garbage5.jpg", "FATIMA Hotspot", "Uncleaned", 1709057600, "user_987",2),
+        UserReport("report_128", 37.4220, -122.0841, "https://example.com/garbage5.jpg", "FATIMA Hotspot", "Uncleaned", 1709057600, "user_987",2),
     )
 
     fun allReports(): List<UserReport> = reports
