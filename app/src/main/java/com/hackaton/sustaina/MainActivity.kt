2a7ef53a86b6f5a75ce0b85b5446cca7f4100282
Clorky <clarkisnoob434@gmail.com>
2025-03-01 12:47:29 +0800

    Cleaned up unused imports.

--- app/src/main/java/com/hackaton/sustaina/MainActivity.kt
+++ app/src/main/java/com/hackaton/sustaina/MainActivity.kt
@@ -4,18 +4,10 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import com.hackaton.sustaina.ui.navigation.MainScreen
-import com.hackaton.sustaina.ui.navigation.Navigation
 import com.hackaton.sustaina.ui.theme.SustainaTheme
 import dagger.hilt.android.AndroidEntryPoint
 
