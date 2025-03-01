e71a680769bb9c90062a6c5d44c1620567b82bbb
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-03-01 22:20:23 +0800

    Refactor: Updated Campaign data model
    Added the `campaignOrganizerId`, `campaignDescription`, `latitude`, and `longitude` fields to the Campaign data model. Also renamed `campaignAbout` to `campaignDescription`.

--- app/src/main/java/com/hackaton/sustaina/domain/models/Campaign.kt
+++ app/src/main/java/com/hackaton/sustaina/domain/models/Campaign.kt
@@ -8,11 +8,14 @@ data class Campaign(
     val campaignId: String = "",
     val campaignAttendingUser: List<String> = listOf(),
     val campaignName: String = "",
+    val campaignOrganizerId: String = "",
     val campaignOrganizer: String = "",
     val campaignStartDate: Long = Instant.now().toEpochMilli(),
-    val campaignAbout: String = "",
+    val campaignDescription: String = "",
     val campaignVenue: String = "",
-    val campaignAddress: String = ""
+    val campaignAddress: String = "",
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0
 )
 
 fun Long.toLocalDateTime(): LocalDateTime {
